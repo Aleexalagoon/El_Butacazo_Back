2abdee2c46f0ba1f28d13993efa60db1254a56c3
Daniel <a27314@svalero.com>
2024-11-12 19:12:49 +0100

    Creación de entradas y películas

---
+++ Models/Entradas.cs
@@ -0,0 +1,24 @@
+namespace Models;
+
+public class Entradas {
+    public static int nextId = 1;
+    public int Id {get; private set;}
+    public Peliculas peliculas {get;set;}
+    public Salas salas {get;set;}
+    public Butacas butacas {get;set;}
+    public string Hora {get;set;}
+    public string Precio {get;set;}
+    public string Cantidad {get;set;}
+
+public Entradas(Peliculas pelicula, Salas sala, Butacas butaca, string hora, string precio, string cantidad) {
+    Id = nextId++;
+    peliculas = pelicula;
+    salas = sala;
+    butacas = butaca;
+    Hora = hora;
+    Precio = precio;
+    Cantidad = cantidad;
+    }
+
+    public abstract void MostrarDetalles();
+}
