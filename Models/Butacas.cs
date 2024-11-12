31e549f28e0973201a20feed93845119993d8592
Aleexalagoon <a27304@svalero.com>
2024-11-12 19:13:28 +0100

    añadido butacas y salas

---
+++ Models/Butacas.cs
@@ -0,0 +1,22 @@
+namespace Models;
+
+public class Butacas {
+    public static int nextId = 1;
+    public int Id {get; private set;}
+    public int Fila {get;set;}
+    public int Columna {get;set;}
+    public bool Estado {get;set;}
+
+
+public Butacas (int fila, int columna) {
+    Id = nextId++;
+    Fila = fila;
+    Columna = columna;
+
+}
+
+    public override void MostrarDetalles(){
+        Console.WriteLine($"Fila:{Fila}, Columna:{Columna} Estado:{Estado}");
+    }
+
+}
